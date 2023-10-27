1720fe3d3abd7701ff7228cb3a1e4deedf055d1c
Donald-KIpchumba <donaldkipchumbaaust@gmail.com>
2023-10-27 09:49:15 +0300

    Added programming_language.py

---
+++ cp1404practicals/prac_06/programming_language.py
@@ -0,0 +1,12 @@
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+    def __str__(self):
+        return f"{self.name}, Dynamic Typing, Reflection={self.reflection}, First appeared in {self.year}"
